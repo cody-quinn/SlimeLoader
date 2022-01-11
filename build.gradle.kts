0dcc46ebc149c508a4eb3bd0faeff813ab25fad1
CatDevz <codyquinn1122@gmail.com>
2022-01-11 17:11:36 -0600

    :sparkles: Switched to use master branch instead of 1.18

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ repositories {
 }
 
 dependencies {
-    compileOnly("com.github.Minestom:Minestom:1.18-SNAPSHOT")
+    compileOnly("com.github.Minestom:Minestom:-SNAPSHOT")
 
     api("com.github.luben:zstd-jni:1.5.0-4")
 }
@@ -27,46 +27,4 @@ val compileKotlin: KotlinCompile by tasks
 compileKotlin.kotlinOptions {
     freeCompilerArgs = listOf("-Xinline-classes")
     jvmTarget = "17"
-}
-
-publishing {
-    repositories {
-        maven("${project.rootDir}/releases")
-    }
-
-    publications {
-        create<MavenPublication>("maven") {
-            groupId = project.group.toString()
-            artifactId = project.name
-            version = project.version.toString()
-
-            from(components["java"])
-
-            pom {
-                packaging = "jar"
-                licenses {
-                    license {
-                        name.set("MIT")
-                        url.set("https://github.com/PhoenixNetwork/SlimeLoader/blob/master/LICENSE")
-                    }
-                }
-            }
-        }
-    }
-
-    repositories {
-        maven {
-            val mavenUsername = project.properties["mavenUser"]
-            val mavenPassword = project.properties["mavenPassword"]
-
-            credentials {
-                username = mavenUsername.toString()
-                password = mavenPassword.toString()
-            }
-
-            val snapshotRepository = uri("https://repo.astromc.gg/repository/maven-snapshots/")
-            val releaseRepository = uri("https://repo.astromc.gg/repository/maven-releases/")
-            url = if (version.toString().endsWith("SNAPSHOT")) snapshotRepository else releaseRepository
-        }
-    }
 }
